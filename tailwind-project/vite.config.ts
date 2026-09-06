26d1f3b0abb506118d333654cf18abb0f5a174c9
Bip-resh <tbipresh27@gmail.com>
2026-09-06 23:13:19 +0600

    work added

--- tailwind-project/vite.config.ts
+++ tailwind-project/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import htmlInject from "vite-plugin-html-inject";
+
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    htmlInject()
   ],
 })
